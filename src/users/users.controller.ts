9148f387e6574e344006a24024cfa150795cbd65
aarao <aarao.melo.lopes@gmail.com>
2021-05-11 22:41:59 -0300

    validation errors interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ErrorsInterceptor } from './validations.interceptor';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseInterceptors(ErrorsInterceptor)
   async create(@Body() createUserDto: CreateUserDto) {
     return await (this.usersService.create(createUserDto));
   }
